b342b7a18566a617f1da3157504ea0c6120e634b
Jack Smith <jack01110111@yahoo.co.uk>
2013-12-13 12:28:36 +0000

    adding dvdrip.pl which uses mplayer to get DVD info and rip a title

---
+++ dvdrip.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use feature qw(say);
+use Getopt::Long;
+
+# Get the name of the .IFO file and the DVD folder from the command line.
+my %args;
+GetOptions(\%args,
+    'i=s',
+    'f=s'
+) or die "Invalid parameters!";
+die "Missing -i switch" unless $args{i};
+die "Missing -f switch" unless $args{f};
+my $ifo=$args{i};
+my $dvdfolder=$args{f};
+
+# Print DVD information to file.
+system("mplayer -identify -frames 0 $ifo > temp.dat");
+
+# Parse file with DVD information and get the number of titles.
+my $noOfTitles;
+my $fin = "temp.dat";
+open (my $fhin,"<",$fin) || die "Couldn't open '".$fin."' for reading because: ".$!;
+while (my $line = <$fhin>) {
+    if (($line =~ /There are/) && ($line =~ /titles on this DVD/)) {
+        $noOfTitles=$line;
+        $noOfTitles=~s/\D//g;
+    }
+}
+close(fhin);
+
+say"Number of titles is: $noOfTitles";
+say"folder=$dvdfolder";
+#system("mplayer -dvd-device $dvdfolder dvd://1");
+system("mplayer -dvd-device $dvdfolder dvd://3 -dumpstream -dumpfile ./3.VOB");
